70646c439ef747dafc84e4022f24fee5a25df5cb
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-05-27 02:00:33 +0500

    ok

--- Models/Direction.cs
+++ Models/Direction.cs
@@ -7,5 +7,8 @@ namespace Speedo_Bus_Facilitation.Models
 {
     public class Direction
     {
+        public int Id { get; set; }
+        public virtual Route route { get; set; }
+        public int routeId { get; set; }
     }
 }
